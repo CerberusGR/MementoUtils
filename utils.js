adae33a89b928e499e440a0c3ead40bbede1bd43
CerberusGR <81939523+CerberusGR@users.noreply.github.com>
2026-04-01 15:50:27 +0300

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,10 @@
+function getGreekDay(dateObj) {
+    var days = ["Κυριακή", "Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκευή", "Σάββατο"];
+    return days[dateObj.getDay()];
+}
+
+function getGreekMonth(dateObj) {
+    var months = ["Ιανουάριος", "Φεβρουάριος", "Μάρτιος", "Απρίλιος", "Μάιος", "Ιούνιος", 
+                  "Ιούλιος", "Αύγουστος", "Σεπτέμβριος", "Οκτώβριος", "Νοέμβριος", "Δεκέμβριος"];
+    return months[dateObj.getMonth()];
+}
